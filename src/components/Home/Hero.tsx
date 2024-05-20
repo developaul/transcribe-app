452ebfda0cd9017447c6118c613547a60287dc0b
Paul Chavez <paulchavezromero20@gmail.com>
2024-05-19 19:06:56 -0500

    chore: Hero section added

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,39 +1,39 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import Link from 'next/link'
+import Image from 'next/image'
+import { TypographyH1, TypographyLarge, TypographyP } from '../ui/Typography'
 
 const Hero = () => {
 
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
-      <div className="container px-4 md:px-6">
-        <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
-          <img
-            alt="Hero"
-            className="mx-auto aspect-video overflow-hidden rounded-xl object-cover sm:w-full lg:order-last lg:aspect-square"
-            height="550"
-            src="/placeholder.svg"
-            width="550"
-          />
-          <div className="flex flex-col justify-center space-y-4">
-            <div className="space-y-2">
-              <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-                Transcribe Your Audio with Ease
-              </h1>
-              <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
-                Transcribe App is a powerful audio transcription tool that delivers accurate and reliable
-                transcripts in minutes.
-              </p>
-            </div>
-            <div className="flex flex-col gap-2 min-[400px]:flex-row">
-              {/* <Link
-                className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                href="/workspace"
-              >
-                Get Started
-              </Link> */}
-            </div>
-          </div>
+    <section className="container h-svh flex flex-col items-center justify-center lg:flex-row gap-8 lg:gap-14">
+      <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-2 items-center justify-center text-center lg:text-left">
+          <TypographyH1>
+            Transcribe Your Audio with Ease
+          </TypographyH1>
+
+          <TypographyLarge>
+            Transcribe App is a powerful audio transcription tool that delivers accurate and reliable transcripts in minutes.
+          </TypographyLarge>
+        </div>
+        <div className='flex items-center justify-center lg:justify-start'>
+          <Link href="/workspace">
+            <Button>
+              Get Started
+            </Button>
+          </Link>
         </div>
       </div>
+
+      <Image
+        src="/desktop.webp"
+        alt="Desktop"
+        width={550}
+        height={550}
+        className="object-cover rounded-xl"
+      />
     </section>
   )
 
